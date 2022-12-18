6ba58d6d0d1bc6ed379e465db6fbf7d66a76760a
Luis Ayala <saltwaterlou@yahoo.com>
2022-12-18 11:03:28 -0600

    Updates to score and timer

--- end.js
+++ end.js
@@ -2,6 +2,10 @@ const username = document.getElementById('username');
 const saveScoreBtn = document.getElementById('saveScoreBtn');
 const finalScore = document.getElementById('finalScore');
 const mostRecentScore = localStorage.getItem('mostRecentScore');
+const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+
+const MAX_HIGH_SCORES = 5;
+
 finalScore.innerText = mostRecentScore;
 
 username.addEventListener('keyup', () => {
@@ -10,4 +14,16 @@ username.addEventListener('keyup', () => {
 
 saveHighScore = (e) => {
     e.preventDefault();
+
+    const score = {
+        score: mostRecentScore,
+        name: username.value,
+    };
+    highScores.push(score);
+    highScores.sort((a, b) => b.score - a.score);
+    highScores.splice(5);
+
+    localStorage.setItem('highScores', JSON.stringify(highScores));
+    window.location.assign('/');
 };
+
--- game.js
+++ game.js
@@ -1,61 +1,64 @@
-const question = document.getElementById('question');
-const choices = Array.from(document.getElementsByClassName('choice-text'));
-const timer = document.querySelector('#time-left');
+const questions = document.getElementById('question')
+const choices = Array.from(document.getElementsByClassName('choice-text'))
+const questionCounterText = document.getElementById('questionCounter')
+const scoreText = document.getElementById('score')
+const timer = document.querySelector('#time-left')
 
-let currentQuestion = {};
-let acceptingAnswers = false;
-let score = 0;
-let questionCounter = 0;
-let availableQuesions = [];
-let time = 15;
+let currentQuestion = {}
+let acceptingAnswers = false
+let score = 0
+let questionCounter = 0
+let availableQuestions = []
+let time = 15
 
-let questions = [
+let question = [
     {
-        question:  'JavaScript is a   -side programming language?',
+        question:"JavaScript is a   -side programming language?",
         choice1: 'Client',
         choice2: 'Server',
         choice3: 'Both',
         choice4: 'None',
-        answer: 3,
+        answer: 3
     },
     {
-        question:
-            "Which of the following will write the message 'Hello DataFlair!' in an alert box?",
-        choice1: "alertbox('Hello DataFlar!');",
-        choice2: "alert(Hello DataFlair!);",
-        choice3: "msgAlert('Hello DataFlair!);",
-        choice4: "alert('Hello DataFlair!);",
-        answer: 4,
+        question: "Which of the following will write the message 'Hello DataFlair!' in an alert box?",
+        choice1: "alertbox('Hello DataFlar!')",
+        choice2: "alert(Hello DataFlair!)",
+        choice3: "msgAlert('Hello DataFlair!)",
+        choice4: "alert('Hello DataFlair!)",
+        answer: 4
     },
     {
-        question: " Which are the correct 'if' statements to execute certain code if 'x' is equal to 2?",
+        question: "Which are the correct 'if' statements to execute certain code if 'x' is equal to 2?",
         choice1: "if(x 2)",
         choice2: "if(x=2)",
         choice3: "if(x==2);",
         choice4: "if(x !=2);",
-        answer: 3,
+        answer: 3
     },
     {
-        question: " How do you find the minimum of x and y using Javascript?",
+        question: "How do you find the minimum of x and y using Javascript?",
         choice1: "min(x,y)",
         choice2: "Math.min(x,y)",
         choice3: "Math.min(xy)",
         choice4: "min(xy)",
-        answer: 2,
-    },
-];
+        answer: 2
+    }
+]
 
 //CONSTANTS
-const CORRECT_BONUS = 10;
-const MAX_QUESTIONS = 4;
+const CORRECT_BONUS = 10
+const MAX_QUESTIONS = 4
+const TIMER_UP = 5
+const TIMER_DOWN = 10
 
 startGame = () => {
-    questionCounter = 0;
-    score = 0;
-    availableQuesions = [...questions];
+    questionCounter = 0
+    score = 0
+    availableQuestions = [...question]
     getNewQuestion()
-    countdown_start();
-};
+}
+
 countdown_start = () => {
     time = 15
         const gameTimer = setInterval(function() {
@@ -69,34 +72,65 @@ countdown_start = () => {
             }
         }, 1000)
 }
+
 getNewQuestion = () => {
-    if (availableQuesions.length === 0 || questionCounter >= MAX_QUESTIONS) {
-        //go to the end page
-        return window.location.assign('/end.html');
+    if (availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS) {
+    
+        localStorage.setItem('mostRecentScore', score)
+        // go to the end page
+        return window.location.assign('/end.html')
     }
     questionCounter++;
-    const questionIndex = Math.floor(Math.random() * availableQuesions.length);
-    currentQuestion = availableQuesions[questionIndex];
-    question.innerText = currentQuestion.question;
+    questionCounterText.innerText = questionCounter + "/" + MAX_QUESTIONS
+    
+    const questionIndex = Math.floor(Math.random() * availableQuestions.length)
+    currentQuestion = availableQuestions[questionIndex]
+    question.innerText = currentQuestion.question
 
     choices.forEach((choice) => {
         const number = choice.dataset['number'];
-        choice.innerText = currentQuestion['choice' + number];
-    });
+        choice.innerText = currentQuestion['choice' + number]
+    })
 
-    availableQuesions.splice(questionIndex, 1);
-    acceptingAnswers = true;
-};
+    availableQuestions.splice(questionIndex, 1)
+    acceptingAnswers = true
+}
 
 choices.forEach((choice) => {
     choice.addEventListener('click', (e) => {
-        if (!acceptingAnswers) return;
+        if (!acceptingAnswers) return
 
         acceptingAnswers = false;
         const selectedChoice = e.target;
-        const selectedAnswer = selectedChoice.dataset['number'];
-        getNewQuestion();
-    });
-});
+        const selectedAnswer = selectedChoice.dataset['number']
 
-startGame();
+        const classtoApply = 
+        selectedAnswer == currentQuestion.answer ? "correct" : "incorrect"
+        
+        if(classtoApply === 'correct') {
+            incrementScore(CORRECT_BONUS)
+        }
+        selectedChoice.parentElement.classList.add(classtoApply)
+
+        setTimeout(() => {
+          selectedChoice.parentElement.classList.remove(classtoApply)
+          getNewQuestion()
+        }, 1000)
+      })
+    })
+    
+    incrementScore = num => {
+      score += num;
+      scoreText.innerText = score
+    }
+
+    incrementTime = num => {
+        time += num
+        timer.innerText = time
+    }
+
+    decrementTime = num => {
+        time -= num
+        timer.innerText = time
+    }
+    startGame()
